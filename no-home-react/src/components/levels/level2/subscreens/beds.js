b3c6f4d247cc20309e4ed15342df05eba2b75356
Big Paws <crowstormofficial@gmail.com>
2017-11-17 12:21:56 +0100

    hidden divs ready

--- no-home-react/src/components/levels/level2/subscreens/beds.js
+++ no-home-react/src/components/levels/level2/subscreens/beds.js
@@ -6,6 +6,10 @@ export default class Beds extends React.Component{
         const changeSublevel = this.props.changeSublevel
         return(
             <div id='beds'>
+                <div id='leftBed' />
+                <div id='rightBed' />
+                <div id='boltcutters' />
+                
                 <img className="down" src={require("../../../../assets/down.png")} onClick={() => changeSublevel('Corridor')} alt="" />
             </div>
         )
